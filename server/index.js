b8f3f0c175fed1870576d34e0c10e27562e89f03
jongregtom <jonthomas83@gmail.com>
2019-02-22 13:10:17 -0800

    modified port variable

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 const path = require('path');
 
 var app = express();
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
