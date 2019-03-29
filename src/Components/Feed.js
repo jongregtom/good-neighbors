1788bd5f11d0e57a5bd7c38fc4a009a5b95ab6b6
jongregtom <jonthomas83@gmail.com>
2019-03-29 13:39:40 -0700

    checking $PORT var in prod

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -36,7 +36,7 @@ const Feed = function(props) {
     const { classes } = props;
 
     const getRequests = () => {
-        console.log('process.env: ', process.env)
+        console.log('$PORT', $PORT)
         var query = `query {
             getRequests {
                 id
